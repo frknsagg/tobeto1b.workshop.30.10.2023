80c5be1f4346f0a99d9cd89736aa99c9ccdf6bab
frknsagg <furkansag_21@hotmail.com>
2023-10-30 15:35:33 +0300

    Anagram

---
+++ Anagram/src/main/java/org/example/Anagram.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String series1 = "rkaı ";
+        String series2 = " rakı";
+        anagramChecker(series1,series2);
+        if (anagramChecker(series1,series2)){
+            System.out.println("iki string birbirinin anagramıdır.");
+        }
+        else {
+            System.out.println("iki string birbirinin anagramı değildir.");
+        }
+    }
+    public static boolean anagramChecker(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+        char[] a1 = string1.toCharArray();
+        char[] a2 = string2.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        //Karakterleri ASCI kodlarına göre sıralıyor.
+
+        return Arrays.equals(a1, a2);
+    }
+}
